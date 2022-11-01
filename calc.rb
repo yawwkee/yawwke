c2303b6394f45d9e36448f68dc87739202bd768c
yawwkee <liltoster123@gmail.com>
2022-11-01 18:18:24 +0000

    yawwkee

---
+++ calc.rb
@@ -0,0 +1,14 @@
+#Data types
+
+#float
+#boolean
+#String
+#integer
+puts 'write amount of kilometers'
+@kilometers_data = gets.to_f
+
+def calculator_of_kilometers
+  @kilometers_data * 1000.0
+end 
+
+puts "*#{calculator_of_kilometers} m"
