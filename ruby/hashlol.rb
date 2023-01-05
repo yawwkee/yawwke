8cba242cb72f65e2c07a099cf0bb6c3e2fb4bb53
yawwkee <liltoster123@gmail.com>
2023-01-05 19:06:06 +0000

    hashlol

---
+++ ruby/hashlol.rb
@@ -0,0 +1,4 @@
+h1 = {1 => 'one',2 => 'two',3 =>'five',4 => 'four',5 =>'two'}
+h2 = {3 =>'three',5 => 'five'}
+h3 = h1.merge(h2)
+puts h3
--- ruby/opam.rb
+++
@@ -1,4 +0,0 @@
-puts"enter your number of house,my number 15"
-a = gets.to_i
-b = gets.to_i
-a==15||b==15? (puts"here is my number of house") : (puts"here isn't my number of house")
